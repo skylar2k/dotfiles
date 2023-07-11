6728938ff3bd385cae2ec8449238b8c637e8ec31
Olav-André Johansen <oarj@silkstreet.org>
2023-07-11 22:17:22 +0200

    test lua-format

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -1,36 +1,26 @@
 -- Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-  		init = function()
-  		  vim.o.timeout = true
-  		  vim.o.timeoutlen = 300
-  		end
-	},
-	{
-		"nvim-telescope/telescope.nvim", tag = "0.1.2",
-		dependencies = { "nvim-lua/plenary.nvim" }
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end
+    }, {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
+        dependencies = {"nvim-lua/plenary.nvim"}
+    }, {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}}
 })
 
 -- Variables
@@ -41,16 +31,13 @@ local ts = require('telescope.builtin')
 vim.g.mapleader = " "
 
 wk.register({
-	f = {
-		name = "File",
-		f = { ts.find_files, "Find file" },
-		g = { ts.live_grep, "Live grep" },
-	},
-	b = {
-		name = "Buffer",
-		b = { ts.buffers, "Open buffers" },
-	},
-}, { prefix = "<leader>" })
+    f = {
+        name = "File",
+        f = {ts.find_files, "Find file"},
+        g = {ts.live_grep, "Live grep"}
+    },
+    b = {name = "Buffer", b = {ts.buffers, "Open buffers"}}
+}, {prefix = "<leader>"})
 
 -- Search
 vim.opt.ignorecase = true
@@ -58,7 +45,7 @@ vim.opt.smartcase = true
 
 -- Visual
 vim.opt.relativenumber = true
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
 
 -- Tab
 vim.opt.tabstop = 4
